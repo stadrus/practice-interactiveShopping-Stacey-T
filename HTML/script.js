69f803472205532b4b2dd47e7d653e9bfc42f9c3
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-05-18 18:49:38 -0500

    Not finished

--- HTML/script.js
+++ HTML/script.js
@@ -1,42 +1,53 @@
-let cart = [];
-let item = "";
-// console.log(cart);
-// 
+// const cart = [
+//   { name: "Laptop", price: 1000 },
+//   { name: "Phone", price: 500 },
+//   { name: "Headphones", price: 200 }
+// ];
 
-const addItemInput = document.getElementById("add-item");
+// const form = document.querySelector('form');
+
+const addItemInput = document.getElementById("item-input");
 const addItemButton = document.getElementById("add");
 const removeItemButton = document.getElementById("remove");
 const editItemButton = document.getElementById("edit");
-const shoppingCart = document.getElementById("cart").innerHTML;
+const shoppingList = document.getElementById("list")
 
 
 
-function addItems(item){
-    
-    document.getElementById("cart").innerHTML = shoppingCart;
-        docoument.getElementById("add-item") = cart.push(item);
-            return cart;
-}
-console.log(addItems("juice"));
+
+
+window.onload = () => {  
+        document.addEventListener("click", (addItem));
+        
+        function addItem () {
+            document.getElementById("add").innerHTML;
+            document.getElementById("item-input") = cart.push(item);
+        };
+        console.log(addItem("juice"));
+
+        
+
 
 function removeItems(cart, item){
-   let index = cart.indexOf(item);
-   let newCart = index !== -1?
-   [...cart.slice(0,index), ...cart.slice(index +1 )]: item;
-   return newCart;
-
-}
-// console.log("Updated cart:", removeItems());
-
-
-// function editItems(item){
-//     let updatedItem = cart.map(item => item === item)
-//     return updatedItem; 
-// }
-// console.log(editItems())
-// console.log(cart);
-
-addItemButton.addEventListener("click", addItems);
-addItemInput.addEventListener("load", addItems);
-// removeItemButton.addEventListener("click", removeItems);
-// editItemButton.addEventListener("click", editItems);
+           let index = cart.indexOf(item);
+                   let newCart = index !== -1?
+                   [...cart.slice(0,index), ...cart.slice(index +1 )]: item;
+                   return newCart;
+                
+                }
+                console.log("Updated cart:", removeItems());
+                
+                
+                function editItems(item){
+                            let updatedItem = cart.map(item => item === item)
+                            return updatedItem; 
+                        }
+                        
+                        
+                        
+                }
+                        console.log(editItems())
+                        console.log(cart);
+                        
+                        removeItemButton.addEventListener("click", removeItems);
+                        editItemButton.addEventListener("click", editItems);
